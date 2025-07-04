d38d00240fc9b51f09fa3ec39b6efcfaa3a39695
Yogesh Pote <yogpote035@gmail.com>
2025-07-04 22:02:45 +0530

    Fix: force copy _redirects for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-        tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects",
+          dest: ".", // copy to dist root
+        },
+      ],
+    }),
   ],
-})
+});
