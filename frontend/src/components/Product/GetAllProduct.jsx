1af1f35bc135464a86d63e1a6638dbe83f42212e
Yogesh Pote <yogpote035@gmail.com>
2025-07-04 21:49:45 +0530

    update _redirects

--- frontend/src/components/Product/GetAllProduct.jsx
+++ frontend/src/components/Product/GetAllProduct.jsx
@@ -17,16 +17,15 @@ function GetAllProducts() {
     : products;
 
   return (
-    <div className="max-w-7xl mx-auto px-4 py-10">
-      <h2 className="text-4xl font-bold text-center text-indigo-700 mb-6">
-        Explore Rental's
-      </h2>
-
+    <div className="max-w-7xl mx-auto px-4 py-2">
       {/* 🔹 Category Filter Navigation */}
       <CategoryNavbar
         selected={selectedCategory}
         onSelect={setSelectedCategory}
       />
+      <h2 className="text-4xl font-bold text-center text-indigo-700 mb-6">
+        Explore Rental's
+      </h2>
 
       {/* 🔹 Filtered Product List */}
       {filteredProducts?.length === 0 ? (
