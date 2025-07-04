a3526e4b14d3bb50237f5966b84ce65e90a3b8d4
Yogesh Pote <yogpote035@gmail.com>
2025-07-04 22:06:45 +0530

    Update vite.config.js
    letter solve thi sissue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,10 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/_redirects",
-          dest: ".", // copy to dist root
-        },
-      ],
-    }),
+  plugins: [react(),
+        tailwindcss(),
   ],
-});
+})
