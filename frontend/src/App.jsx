91574bb3bd7dc0e7504e1ed12b260ad47ae86f9a
Yogesh Pote <yogpote035@gmail.com>
2025-07-05 17:40:08 +0530

    state change for nav and footer in chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,117 +1,117 @@
-import Login from "./components/Authentication/Login";
-import "./App.css";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// src/App.jsx
+import { useLocation, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+// Context Providers
+import OrderState from "./context/orders/OrderState";
+import CartState from "./context/cart/CartState";
+import ProductState from "./context/Product/ProductState";
+import UserState from "./context/Authentication/UserState";
+
+// Pages and Components
+import Login from "./components/Authentication/Login";
 import Signup from "./components/Authentication/Signup";
 import CreateProduct from "./components/Product/CreateProduct";
-import UserState from "./context/Authentication/UserState";
-import ProductState from "./context/Product/ProductState";
 import GetAllProduct from "./components/Product/GetAllProduct";
 import GetOneProduct from "./components/Product/GetOneProduct";
 import UpdateProduct from "./components/Product/UpdateProduct";
-import Navbar from "./components/Navbar";
 import MyProducts from "./components/Product/MyProducts";
-import ProtectedRoutes from "./components/ProtectedRoutes";
 import MyCart from "./components/Cart/MyCart";
-import CartState from "./context/cart/CartState";
 import MyOrder from "./components/Cart/MyOrder";
-import OrderState from "./context/orders/OrderState";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import SearchProduct from "./components/Product/SearchProduct";
 import AboutPage from "./pages/AboutPage";
 import Footer from "./pages/Footer";
+import Navbar from "./components/Navbar";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 import ChatRoom from "./components/chat/ChatRoom";
+
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+
 function App() {
+  const location = useLocation();
+  const isChatPage = location.pathname === "/chat";
+
   return (
     <OrderState>
       <CartState>
         <ProductState>
           <UserState>
-            <Router>
-              <div className="min-h-screen flex flex-col">
-                {window.location.pathname !== "/chat" && <Navbar />}
-                {/* Main Content */}
-                <div className="flex-grow">
-                  <Routes>
-                    <Route exact path="/login" element={<Login />} />
-                    <Route exact path="/signup" element={<Signup />} />
-                    <Route exact path="/" element={<GetAllProduct />} />
-                    <Route
-                      exact
-                      path="/product/:id"
-                      element={
-                        <LocalizationProvider dateAdapter={AdapterDateFns}>
-                          <ProtectedRoutes>
-                            <GetOneProduct />
-                          </ProtectedRoutes>
-                        </LocalizationProvider>
-                      }
-                    />
-                    <Route
-                      exact
-                      path="/create-product"
-                      element={
-                        <ProtectedRoutes>
-                          <CreateProduct />
-                        </ProtectedRoutes>
-                      }
-                    />
-                    <Route
-                      exact
-                      path="/update-product/:id"
-                      element={
-                        <ProtectedRoutes>
-                          <UpdateProduct />
-                        </ProtectedRoutes>
-                      }
-                    />
-                    <Route
-                      exact
-                      path="/my-products"
-                      element={
-                        <ProtectedRoutes>
-                          <MyProducts />
-                        </ProtectedRoutes>
-                      }
-                    />
-                    <Route
-                      exact
-                      path="/my-favorite"
-                      element={
-                        <ProtectedRoutes>
-                          <MyCart />
-                        </ProtectedRoutes>
-                      }
-                    />
-                    <Route
-                      path="/my-rentals"
-                      element={
-                        <ProtectedRoutes>
-                          <MyOrder />
-                        </ProtectedRoutes>
-                      }
-                    />
-                    <Route path="/search" element={<SearchProduct />} />
-                    <Route path="/about-dev" element={<AboutPage />} />
-                    <Route
-                      path="/chat"
-                      element={
-                        <ProtectedRoutes>
-                          <ChatRoom />
-                        </ProtectedRoutes>
-                      }
-                    />
-                  </Routes>
-                </div>
+            <div className="min-h-screen flex flex-col">
+              {!isChatPage && <Navbar />}
 
-                {window.location.pathname !== "/chat" && <Footer />}
-                <ToastContainer position="bottom-left" autoClose={3000} />
+              <div className="flex-grow">
+                <Routes>
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="/" element={<GetAllProduct />} />
+                  <Route
+                    path="/product/:id"
+                    element={
+                      <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <ProtectedRoutes>
+                          <GetOneProduct />
+                        </ProtectedRoutes>
+                      </LocalizationProvider>
+                    }
+                  />
+                  <Route
+                    path="/create-product"
+                    element={
+                      <ProtectedRoutes>
+                        <CreateProduct />
+                      </ProtectedRoutes>
+                    }
+                  />
+                  <Route
+                    path="/update-product/:id"
+                    element={
+                      <ProtectedRoutes>
+                        <UpdateProduct />
+                      </ProtectedRoutes>
+                    }
+                  />
+                  <Route
+                    path="/my-products"
+                    element={
+                      <ProtectedRoutes>
+                        <MyProducts />
+                      </ProtectedRoutes>
+                    }
+                  />
+                  <Route
+                    path="/my-favorite"
+                    element={
+                      <ProtectedRoutes>
+                        <MyCart />
+                      </ProtectedRoutes>
+                    }
+                  />
+                  <Route
+                    path="/my-rentals"
+                    element={
+                      <ProtectedRoutes>
+                        <MyOrder />
+                      </ProtectedRoutes>
+                    }
+                  />
+                  <Route path="/search" element={<SearchProduct />} />
+                  <Route path="/about-dev" element={<AboutPage />} />
+                  <Route
+                    path="/chat"
+                    element={
+                      <ProtectedRoutes>
+                        <ChatRoom />
+                      </ProtectedRoutes>
+                    }
+                  />
+                </Routes>
               </div>
-            </Router>
+
+              {!isChatPage && <Footer />}
+              <ToastContainer position="bottom-left" autoClose={3000} />
+            </div>
           </UserState>
         </ProductState>
       </CartState>
