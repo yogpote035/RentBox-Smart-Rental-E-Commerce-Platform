870be6d740606678b714b18ddab7bdbbc3522f34
Yogesh Pote <yogpote035@gmail.com>
2025-07-06 09:44:52 +0530

    Center align My Rental Orders heading

--- frontend/src/components/Cart/MyOrder.jsx
+++ frontend/src/components/Cart/MyOrder.jsx
@@ -60,7 +60,7 @@ function MyOrder() {
 
   return (
     <div className="max-w-7xl mx-auto px-4 py-10">
-      <h2 className="text-4xl font-bold text-indigo-700 mb-8 border-b pb-2">
+      <h2 className="text-4xl text-center font-bold text-indigo-700 mb-8 border-b pb-2">
         My Rental Orders
       </h2>
 
